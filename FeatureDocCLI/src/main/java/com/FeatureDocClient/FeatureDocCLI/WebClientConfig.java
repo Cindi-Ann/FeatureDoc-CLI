15fbadcf179b42b314a1472a5cc4ad34c2796eaf
Keith Hughes <keith.hughes@bbd.co.za>
2025-03-18 20:42:36 +0200

    add config to allow redirect when we get a redirect from google

--- FeatureDocCLI/src/main/java/com/FeatureDocClient/FeatureDocCLI/WebClientConfig.java
+++ FeatureDocCLI/src/main/java/com/FeatureDocClient/FeatureDocCLI/WebClientConfig.java
@@ -2,16 +2,45 @@ package com.FeatureDocClient.FeatureDocCLI;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient webClient() {
-        return WebClient.builder()
+
+        HttpClient httpClient = HttpClient.create()
                 .baseUrl("http://localhost:8080")
-                .defaultHeader("Accept", "application/json")
+                .followRedirect(true)
+                .doOnRedirect((httpClientResponse, connection) -> {
+                    System.out.println(httpClientResponse.status());
+                    if (httpClientResponse.responseHeaders().getAsString("Location").contains("accounts.google.com")) {
+
+                        if (Desktop.isDesktopSupported()) {
+
+                            Desktop desktop = Desktop.getDesktop();
+                            try {
+                                desktop.browse(URI.create(httpClientResponse.responseHeaders().getAsString("Location")));
+                            } catch (IOException e) {
+                                System.out.println(e.getMessage());;
+                            }
+
+                        }
+                    }
+                    System.out.println(httpClientResponse.responseHeaders().entries());
+                })
+                ;
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .defaultHeader("Accept", "text/html")
                 .build();
     }
 }
